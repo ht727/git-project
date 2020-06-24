eae8ad314ecfc4ddafe921c20d55534dd03e5f96
hlt_keepdream <hlt_keepdream@163.com>
2020-06-24 13:53:46 +0800

    创建工程：git-test hello hlt hhhh

--- src/main/java/com/huangliutan/GitTest.java
+++ src/main/java/com/huangliutan/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello hlt666");
+        System.out.println("hello hhhhh");
     }
 }
